94d44fe7b7d9e3974c728559edd6e22601d44231
Pascal Tribel <ptribel@ulb.ac.be>
2022-12-25 23:40:11 +0100

    wavelet

--- src/misceallaneous.py
+++ src/misceallaneous.py
@@ -8,8 +8,8 @@ import librosa.display
 def getWavFileAsNpArray(filename):
     return np.array(wavfile.read(filename)[1])
 
-def displaySpectrogram(stft_array, axe=0):
-    S_db = librosa.amplitude_to_db(np.abs(stft_array), ref=np.max)
+def displaySpectrogram(stft_array, sr=12000, axe=0):
+    #S_db = librosa.amplitude_to_db(np.abs(stft_array), ref=np.max)
     if axe == 0:
         axe = plt.subplot(1, 1, 1)
-    librosa.display.specshow(S_db, ax=axe)
+    librosa.display.specshow(stft_array, sr=sr, ax=axe)
